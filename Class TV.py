62d83051f89f8f1cee04f13f6d70dd9f7530f047
unknown <rv8828120@gmail.com>
2023-05-15 21:53:19 +0800

    Class TV

--- Class TV.py
+++ Class TV.py
@@ -2,7 +2,12 @@
 #BSCPE 1-4
 #Object-Oriented Programming
 
-#Constructor
+class TV:
+    #Constructor
+    def __init__(self):
+        self.channel = 0
+        self.volume = 0
+        self.on = True
     #Turns ON this TV
     #Turns OFF this TV
     #Get Channel
