731f8cab3041d30b40517bbf0cb922562488cead
unknown <rv8828120@gmail.com>
2023-05-16 14:06:49 +0800

    Test Driver Program for TV 1

--- TestTV.py
+++ TestTV.py
@@ -1,10 +1,18 @@
+from ClassTV import TV
+
 #Test Driver Program
+def TestTV():
     #Class TV 1
+    tv1 = TV()
     #Set TV 1's channel and volume
+    tv1.setChannel(30)
+    tv1.setVolume(3)
     #Print the output
+    print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
 
     #Class TV 2
     #Set TV 2's channel and volume
     #Print the output
 
 #For the test cases to be executed, execute the TestTV function.
+TestTV()
