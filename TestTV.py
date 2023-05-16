82badf69fabeeea89b5ac485f1827de64e4a516a
unknown <rv8828120@gmail.com>
2023-05-16 14:11:36 +0800

    Test Driver Program for TV 2

--- TestTV.py
+++ TestTV.py
@@ -11,8 +11,12 @@ def TestTV():
     print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
 
     #Class TV 2
+    tv2 = TV()
     #Set TV 2's channel and volume
+    tv2.setChannel(3)
+    tv2.setVolume(2)
     #Print the output
+    print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())
 
 #For the test cases to be executed, execute the TestTV function.
 TestTV()
